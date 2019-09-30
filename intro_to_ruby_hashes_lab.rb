5777383fad41e74b1c6c794cd82e252d85e163f3
Winifred Frierson <winifred.dykes@hotmail.com>
2019-09-30 05:10:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,15 +46,14 @@ def update_counting_hash(hash, key)
     :apples => 3
   }
   
-  update_counting_hash["apples"] += 1
   
   if update_counting_hash["apples"]
   update_counting_hash["apples"] += 1
 else
   update_counting_hash["apples"] = 1
+  
 end
   
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
-end
